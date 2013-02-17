93552069338b7f59226a987bc87b1574bbe77dc9
Karen Chaw <chaw@lnxsrv03.seas.ucla.edu>
2013-02-17 11:46:49 -0800

    I only pushed on what i worked on. Make sure to copy this to the http-proxy.cc file to run it. Also, be sure to save a copy of the original. I might have messed some things up...

--- http-proxy_KC.cc
+++ http-proxy_KC.cc
@@ -22,11 +22,12 @@ using namespace std;
 const int MAX_THREADS = 10; //maximum number of processes (or threads) we are allowed
 std::map<string,string> cache;
 
-string readAndWrite(void* fd);
+void* readAndWrite(void* fd);
 bool CheckCache(string URL);
+string getResponse(char* request,int socketFd, size_t length);
 
 
-string readAndWrite(void* fd)
+void* readAndWrite(void* fd)
 {
 	string clientBuffer;
 	int* clientfd = (int*) fd;
@@ -42,9 +43,12 @@ string readAndWrite(void* fd)
 		}
 		clientBuffer.append(buf);
 	}
-	while (memmem(clientBuffer.c_str(), clientBuffer.length(), "\r\n\r\n", 4) == NULL); //TODO: Check if this is being called correcly
+	while (memmem(clientBuffer.c_str(), clientBuffer.length(), "\r\n\r\n", 4) == NULL);
+         //TODO: Check if this is being called correcly
 		//http://www.thinkage.ca/english/gcos/expl/c/lib/memmem.html
 
+        cout << "Testing print of clientBuffer..." << endl << clientBuffer << endl << "end";
+        
 		//Parse the request using the given parsing library found in http-request.cc
 	HttpRequest clientReq;
 	try {
@@ -74,6 +78,9 @@ string readAndWrite(void* fd)
 	
 	// HTTP 1.0 needs Connection: close header if not already there
 	string version = clientReq.GetVersion();
+
+        cout << "version = " << version << endl;
+
 	if( version == "1.0")
 		clientReq.ModifyHeader("Connection", "close");
 	
@@ -91,8 +98,21 @@ string readAndWrite(void* fd)
 	{		
 			//format request to send to server
 		size_t requestLength = clientReq.GetTotalLength() + 1;// extra one for \0
-		char *formattedReq = (char *) malloc(requestLength);
-		formattedReq = clientReq.FormatRequest(formattedReq);
+                string fReqStr;
+                cout << "requestLength = " << requestLength << endl;
+		char *formattedReq = new char[requestLength];//(char *) malloc(requestLength);
+		fReqStr = clientReq.FormatRequest(formattedReq);
+
+
+                cout << "fReq Str " << fReqStr << endl;
+               // cout << "formattedReq length = " ;
+                /*int fReqSize = strlen(formattedReq);
+                cout << fReqSize << " " << endl;
+
+                for(int i = 0; i < fReqSize; i++)
+                {
+                        cout << formattedReq[i];
+                }*/
 
 			// If host not specificed then find in the headers
 		string remoteHost;
@@ -111,12 +131,19 @@ string readAndWrite(void* fd)
 		struct addrinfo hints, *res;
 		int toServerFD; //socket between proxy and server
 
-		memset(&hints, 0, sizeof hints);
+		memset(&hints, 0, sizeof(hints));
 		hints.ai_family = AF_UNSPEC;
 		hints.ai_socktype = SOCK_STREAM;
 
 		if(getaddrinfo(host, port, &hints, &res)!=0)
-			return "400 - Bad Request\n\n";
+			//return "400 - Bad Request\n\n";
+
+                cout << "res->ai_family = " << res->ai_family << endl;
+                cout << "res->ai_socktype = " << res->ai_socktype << endl;
+                cout << "res->ai_protocol = " << res->ai_protocol << endl;
+                cout << "Host = " << host << endl;
+                cout << "Port = " << port << endl;
+                
 
 			//create socket and connect
 		toServerFD = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
@@ -127,15 +154,33 @@ string readAndWrite(void* fd)
 		if (connect(toServerFD,res->ai_addr, res->ai_addrlen) < 0) {
 			cerr << "ERROR: Cannot connect." << endl;
 		}
+
+                cout << "I have reached here!" << endl;
 		
 		//TODO: Add the call of the get response function passing in toServerFD file descriptor
-		
+                response = getResponse(formattedReq,toServerFD,requestLength);
+
+                cout << "I have reached after getResponse." << endl;
+
+                if(send(*clientfd, response.c_str(), requestLength, 0))
+                {
+                        
+                };
+                close(toServerFD);                
+
+		return NULL;
+        }
 			//write and send request
-		char *recBuf = new char [1024];
+		/*char *recBuf = new char [1024];
 		char *sendBuf = new char [requestLength+1];
 		if(write(toServerFD, sendBuf, requestLength)<0)
 			cerr<<"Error: Cannot write to socket."<<endl;
-			
+		
+                cout << "I have reached pass the write." << endl;
+
+                int lengthOfSendBuf = strlen(sendBuf);
+                cout << "length of sendBuf = " << lengthOfSendBuf << endl;
+                
 			//read in response
 		int canRead;
 		string message;
@@ -150,7 +195,9 @@ string readAndWrite(void* fd)
 
 		}while(canRead>0);
 
-		//store "message" value in cache
+                cout << "message = " << message << endl;
+
+		//TODO: store "message" value in cache
 
 			//Clean up
 		freeaddrinfo(res);
@@ -168,7 +215,7 @@ string readAndWrite(void* fd)
 			
 		
 		
-		/*if(version = "1.0" or server header has close connection){ //close socket if HTTP/1.0 and reconnect
+		if(version = "1.0" or server header has close connection){ //close socket if HTTP/1.0 and reconnect
 			close(toServerFD);
 		
 			toServerFD = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
@@ -180,13 +227,15 @@ string readAndWrite(void* fd)
 				cerr<<"ERROR, cannot connect"<<endl;
 			}
 				
-		}*/
+		}//*/
 		
 		
 		
-	}
+	/*}
 	
 	pthread_exit(NULL);
+        return NULL;*/
+        return NULL;
 
 }
 
@@ -200,10 +249,10 @@ bool CheckCache(string URL)
 	if (found == cache.end())
 	{
                 return false;
-}
-	else  //check to see if the cached URL has expired or not
+        }
+	/*else  //check to see if the cached URL has expired or not
 	{
-	        if ((cache[URL]).findHeader("Expires")== "")
+	        if ((cache[URL]).FindHeader("Expires")== "")
 	        {
                         return true;
                 }
@@ -212,7 +261,7 @@ bool CheckCache(string URL)
                 	struct tm tm;
 		        time_t t;
 		        time_t currenttime;
-		        const char* date = cache[URL].findHeader(expires);
+		        const char* date = cache[URL].FindHeader(expires);
 	         
 		        if (strptime(date, "%a, %d %b %Y %H:%M:%S %Z", &tm) != NULL)
 		        {
@@ -230,8 +279,52 @@ bool CheckCache(string URL)
 		        }
 	        }
                 return false;
-	}
+	}*/
+        return false;
 	
+}
+
+
+string getResponse(char* request,int socketFd, size_t length)
+{
+
+        cout << "I have entered getResponse and socketFd = " << socketFd << endl;
+        cout << "Request = " << request << endl;
+	if (send(socketFd, request, length, 0) == -1)
+            {
+		        perror("Error: Send failed");
+		        close(socketFd);
+		        exit(EXIT_FAILURE);
+            }
+
+        cout << "get past if in getResponse" << endl;
+	string response;
+	for (;;)
+	{
+                cout << "get into for loop of getResponse" << endl;
+		char resBuf[1024];
+		
+		// Get data from remote
+		int numRecv = recv(socketFd, resBuf, sizeof(resBuf), 0);
+                cout << "numRecv = " << numRecv << endl;
+                //cout << "past recv in getResponse" << endl;
+		if (numRecv < 0)
+		{
+			perror("Error: Could not get response");
+			close(socketFd);
+			exit(EXIT_FAILURE);
+		}
+		
+		// If we didn't receive anything, we hit the end
+		else if (numRecv == 0)
+			break;
+		
+		// Append the buffer to the response if we got something
+		response.append(resBuf, numRecv);
+	}
+
+        cout << "response = " << response << endl;
+	return response;
 }
 
 int main (int argc, char *argv[])
@@ -251,7 +344,7 @@ int main (int argc, char *argv[])
 	
 	sSockAddr.sin_family = AF_INET; //sin_family instead of sa_family on wiki
 	sSockAddr.sin_addr.s_addr = INADDR_ANY; //may need to switch addresses
-	sSockAddr.sin_port = htons(13553); //port number used for listening; Change to 14805 later
+	sSockAddr.sin_port = htons(13686); //port number used for listening; Change to 14805 later
 	
 	//-----Create socket for connection to server on client's side (cSockAddr)-----
 	struct sockaddr_in cSockAddr;
@@ -307,12 +400,16 @@ int main (int argc, char *argv[])
 		threadNum++;
 		if(threadNum == MAX_THREADS) //10 processes total, so processNum 0 to 9
 		{
-			pthread_join(); //wait until all 10 threads are done and then accept more clients
+			//pthread_join(); //wait until all 10 threads are done and then accept more clients
 			threadNum = 0;
 		}
+               
+                //delete later 
+               // close(socketFD);
+               // break;
 	}
 	
-	pthreads_exit(NULL);
+	//pthreads_exit(NULL);
 	close(socketFD);
 	
 	return EXIT_SUCCESS;
